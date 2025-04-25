940daa4566846cafadeb675a17a0bc90b02a26e2
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2025-04-25 18:16:37 +0530

    moving code out

--- test.py
+++ test.py
@@ -7,5 +7,32 @@
 #     print("end")
 
 
-x, y, z = int(input()), int(input()), int(input())
-print(x, y, z)
+# x, y, z = int(input()), int(input()), int(input())
+# print(x, y, z)
+
+# i = 1
+# isEven = False
+# while i <= 3:
+#     num = int(input(f"Enter even number: (Try-{i}): "))
+#     if num % 2 == 0:
+#         print("Winner")
+#         isEven = True
+#         break
+#     i += 1
+
+# if isEven == False:
+#     print("Loser")
+
+
+# i = 1
+# while i <= 10:
+#     if i % 2 == 0:
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+for x in range(-5, 5):
+    print(x, end=" ")
+
+print()
