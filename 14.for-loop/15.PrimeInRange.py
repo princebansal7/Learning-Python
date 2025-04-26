d5798b412b24261c380067e7a8b02b13a8d3a52d
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2025-04-26 12:27:46 +0530

    prime code updated

--- 14.for-loop/15.PrimeInRange.py
+++ 14.for-loop/15.PrimeInRange.py
@@ -1,11 +1,20 @@
-start = 15
-end = 45
+start = int(input("Enter start value: "))
+end = int(input("Enter end value: "))
+
+# for val in range(start, end + 1):
+#     countDivisor = 0
+#     for i in range(1, val + 1):
+#         if val % i == 0:
+#             countDivisor += 1
+#     if countDivisor == 2:
+#         print(val, end=" ")
+
+
 for val in range(start, end + 1):
-    countDivisor = 0
-    for i in range(1, val + 1):
+    for i in range(2, val):
         if val % i == 0:
-            countDivisor += 1
-    if countDivisor == 2:
+            break
+    else:
         print(val, end=" ")
 
 print()
