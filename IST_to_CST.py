14d4bcb509e242eff62df1fec9c9b5a62d27488b
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2025-05-26 13:23:58 +0530

    comment update

--- IST_to_CST.py
+++ IST_to_CST.py
@@ -1,22 +1,17 @@
 from datetime import datetime
 import pytz
 
-# Define input
-input_str = "Mon 26/05/2025 06:00 AM"
+input_str = "Mon 26/05/2025 11:51 AM"  # update IST time with day and time
 print("IST Time:", input_str)
 
-# Step 1: Parse input to datetime object
 ist_format = "%a %d/%m/%Y %I:%M %p"
 ist_time = datetime.strptime(input_str, ist_format)
 
-# Step 2: Set timezone to IST
 ist_zone = pytz.timezone("Asia/Kolkata")
 ist_time = ist_zone.localize(ist_time)
 
-# Step 3: Convert to CST (US Central Standard Time)
 cst_zone = pytz.timezone("US/Central")
 cst_time = ist_time.astimezone(cst_zone)
 
-# Step 4: Format the output as required
 output_str = cst_time.strftime("%a %d/%m/%Y %I:%M %p")
 print("CST TIME:", output_str)
